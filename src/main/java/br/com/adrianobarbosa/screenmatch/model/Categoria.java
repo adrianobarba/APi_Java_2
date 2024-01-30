f9d928a2d38abd5bcafdc51fc90dd871f01b1869
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-30 17:55:00 -0300

    commit inicial

---
+++ src/main/java/br/com/adrianobarbosa/screenmatch/model/Categoria.java
@@ -0,0 +1,27 @@
+package br.com.adrianobarbosa.screenmatch.model;
+
+import br.com.adrianobarbosa.screenmatch.principal.Principal;
+
+public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    CEMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+}
